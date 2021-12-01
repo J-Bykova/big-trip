ee979daf75c74d851aa0f65a5aa3d3b97fa22e90
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-11-30 19:56:53 -0800

    Add components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-function foo() {
-  console.log('ok');
-}
+import {RenderPosition, renderTemplate} from './render.js';
+import {createMenuTemplate} from './view/menu.js';
+import {createFilterTabsTemplate} from './view/filter-tabs.js';
+import {createEmptyTripPlaceholderTemplate} from "./view/empty-trip-placeholder.js";
 
-foo();
+const header = document.querySelector('.page-header');
+const menu = header.querySelector('.trip-controls__navigation');
+const filters = header.querySelector('.trip-controls__filters');
+const main = document.querySelector('.page-main');
+const events = main.querySelector('.trip-events');
+
+renderTemplate(menu, createMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(filters, createFilterTabsTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(events, createEmptyTripPlaceholderTemplate(), RenderPosition.BEFOREEND);
